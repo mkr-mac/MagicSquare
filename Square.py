8fe8a45b2e3d46107679f7ee35f7596c36dbe989
CorruptData <mccargishm@gmail.com>
2016-04-27 21:19:37 -0400

    Comments?

--- Square.py
+++ Square.py
@@ -1,25 +1,34 @@
 import sys
 
 def magic(size, power):
+	#Get the number of boxes
+	#and a value for giving a starting value to the boxes
 	numbox = initval = size ** 2
+	#The main list of the boxes
 	a = [0] * (numbox)
+	#Highest number to be run through
 	maxsize = (numbox)**power
 	print("Searching...")
+	#Sets initial values for the list
 	for i in xrange(numbox):
 		a[i] = initval
 		initval -= 1
 	
 	a[0] -= 1
+	#Main loop
 	while (a[numbox-1] < maxsize):
 		a[0] += 1
 		rollover(a, numbox-1, maxsize)
+		#Duplicates restart the loop
 		if (checkdubs(a)):
 			continue
+		#Check for solution
 		addemup(a, size, power)
 		
 	return "END"
 
 def rollover(a, numbox_minus, maxsize):
+	#Checking all the numbers to see if they are still in range
 	for l in xrange(numbox_minus):
 		if (a[l] > maxsize):
 			a[l] = 1
@@ -29,15 +38,19 @@ def checkdubs(a):
 	seen = []
 	indx = seen.index
 	for x in a:
+		#See if we've seen x before
 		if x in seen:
+			#If so, advance the first of the numbers
 			a[0] -= 1
 			a[indx(x)] += 1
 			return True
+		#Else Add it to our list
 		seen += [x]
 	return False
 
 def addemup(a, size, power):
 	f = [0] * ((size * 2) + 2)
+	#Do some fancy list stuff
 	for m in xrange(size):
 		for n in xrange(size):
 			f[m] += a[(size*m)+n] ** power
@@ -46,12 +59,14 @@ def addemup(a, size, power):
 		f[(2*size)+1] += a[(m*size)+m] ** power
 	
 	if(allsame(f)):
+		#We did it!
 		print("SOLUTION!!!")
 		for o in a:
 			print(o)
 
 def allsame(f):
 	allf = f[0]
+	#Are all our additions the same?
 	for y in f:
 		if (y != allf):
 			return False
