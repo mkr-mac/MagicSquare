462273821c7c490bf72675b89642e81c94e6d216
CorruptData <mccargishm@gmail.com>
2016-09-24 13:49:21 -0400

    Replaced a loop with some list tricks
    1.25s

--- Square.py
+++ Square.py
@@ -6,23 +6,24 @@ def magic(size, power):
 	#Get the number of boxes
 	numbox = size ** 2
 	#Sets initial values for the list
-	a = [x for x in xrange(1, numbox+1)]
+	a = range(1,numbox+1)
 	fsize = (2*size)+2
 	#Main loop
 	print("Starting...")
+	#Magical permutation function
         preq = permutations(a,numbox)
         print("Permutations Complete!")
 	print("Searching...")
+	print(a[::4])
 	for i in preq:
                 #addemup(item, size, power)
                 f = [0] * fsize
                 #Do some fancy list stuff
-                for m in xrange(size):
-                        for n in xrange(size):
-                                f[m] += i[(size*m)+n] ** power
-                                f[m+size] += i[m+(n*size)] ** power
-                        f[2*size] += i[((m+1)*size)-(m+1)] ** power
-                        f[(2*size)+1] += i[(m*size)+m] ** power
+                for n in xrange(size):
+                        f[n] = sum(i[size*(n):size*(n+1)])
+                        f[size+n] = sum(i[n::size])
+                f[2*size] = sum(i[::size+1])
+                f[2*size+1] = sum(i[size-1:numbox-1:size-1])
                 
                 allf = f[0]
                 #Are all our additions the same?
