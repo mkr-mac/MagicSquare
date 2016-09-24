70d07a58020868a303c154c2e9cd075a23a9a948
CorruptData <mccargishm@gmail.com>
2016-09-24 03:12:45 -0400

    Itertools is the true hero
    Plus, I'm better at Python now.

--- Square.py
+++ Square.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import permutations
 
 def magic(size, power):
 	#Get the number of boxes
@@ -6,60 +7,33 @@ def magic(size, power):
 	numbox = initval = size ** 2
 	#The main list of the boxes
 	a = [0] * (numbox)
-	#Highest number to be run through
-	maxsize = (numbox)**power
-	print("Searching...")
 	#Sets initial values for the list
-	for i in xrange(numbox):
-		a[i] = initval
-		initval -= 1
-	
-	a[0] -= 1
+	a = [x for x in xrange(1, 1000)]
 	#Main loop
-	while (a[numbox-1] < maxsize):
-		a[0] += 1
-		for l in xrange(numbox-1):
-			if (a[l] > maxsize):
-				a[l] = 1
-				a[l+1] += 1
+	print("Starting...")
+        preq = permutations(a,9)
+        print("Permutations Complete!")
+	#q = set(preq)
+	#print("Sets Ready!")
+	print("Searching...")
+	for i in preq:
+                #addemup(item, size, power)
+                f = [0] * ((size * 2) + 2)
+                #Do some fancy list stuff
+                for m in xrange(size):
+                        for n in xrange(size):
+                                f[m] += i[(size*m)+n] ** power
+                                f[m+size] += i[m+(n*size)] ** power
+                        f[2*size] += i[((m+1)*size)-(m+1)] ** power
+                        f[(2*size)+1] += i[(m*size)+m] ** power
+                
+                if(allsame(f)):
+                        #We did it!
+                        print("SOLUTION!!!")
+                        for o in i:
+                                print(o)
 
-		#Duplicates restart the loop
-		if (checkdubs(a)):
-			continue
-		#Check for solution
-		addemup(a, size, power)
-		
 	return "END"
-
-def checkdubs(a):
-	seen = []
-	indx = seen.index
-	for x in a:
-		#See if we've seen x before
-		if x in seen:
-			#If so, advance the first of the numbers
-			a[0] -= 1
-			a[indx(x)] += 1
-			return True
-		#Else Add it to our list
-		seen += [x]
-	return False
-
-def addemup(a, size, power):
-	f = [0] * ((size * 2) + 2)
-	#Do some fancy list stuff
-	for m in xrange(size):
-		for n in xrange(size):
-			f[m] += a[(size*m)+n] ** power
-			f[m+size] += a[m+(n*size)] ** power
-		f[2*size] += a[((m+1)*size)-(m+1)] ** power
-		f[(2*size)+1] += a[(m*size)+m] ** power
-	
-	if(allsame(f)):
-		#We did it!
-		print("SOLUTION!!!")
-		for o in a:
-			print(o)
 
 def allsame(f):
 	allf = f[0]
